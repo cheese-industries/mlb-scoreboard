f41a1507a7ae64db1210de7b90997d1100c69ca7
poutineq <poutineq@gmail.com>
2018-04-23 22:04:11 -0400

    main.js was not complete.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,31 @@
+/* global $, angular */
 'use strict';
-
+ 
 var mlbApp = angular.module('mlbApp', ['scoreboardService']);
-
-mlbApp.controller('scoreboardController', ['$scope', 'getScore',
-
-    function($scope, getScore) {
+ 
+mlbApp.controller('scoreboardController', ['$scope', 'getScore', '$interval',
+ 
+    function($scope, getScore, $interval) {
         $scope.parseInt = parseInt;
         /* The function that updates the scoreboards */
         var updateScore = function(response) {
             var gameData = [];
             if (Array.isArray(response.data.games.game)) {
                 for (var i = 0; i < response.data.games.game.length; i++) {
-        /*
-         * if (response.data.games.game[i].home_team_id == 141 ||
-         * response.data.games.game[i].away_team_id == 141) {
-         * gameData.unshift(response.data.games.game[i]); } else
-         */
-					{
-                        gameData.push(response.data.games.game[i]);
-                    }
+                    gameData.push(response.data.games.game[i]);
                 }
             } else {
                 gameData = [response.data.games.game];
             }
             return gameData;
-        }
-
+        };
+ 
         /* initialize the jQuery calender plugin */
         $("#datepicker").datepicker({
             /* Update the scoreboard whenever a new date is selected */
             onSelect() {
-                var currentDay = new Date($("#datepicker").datepicker("getDate"));
+                // var currentDay = new
+                // Date($("#datepicker").datepicker("getDate"));
                 getScore.get({
                     yearNum: $("#datepicker").datepicker("getDate").getFullYear(),
                     monthNum: ("0" + ($("#datepicker").datepicker("getDate").getMonth() + 1)).slice(-2),
@@ -42,7 +37,7 @@ mlbApp.controller('scoreboardController', ['$scope', 'getScore',
                 });
             }
         });
-
+ 
         /* set the default to today */
         $("#datepicker").datepicker("setDate", (new Date()).toLocaleDateString('en-US'));
         var today = new Date();
@@ -55,8 +50,8 @@ mlbApp.controller('scoreboardController', ['$scope', 'getScore',
         }, function() {
             $('.server-message').attr("style", "display: block");
         });
-
-
+ 
+ 
         /* event handler for choosing previous day */
         $scope.prevDate = function() {
             var prevDay = (new Date($("#datepicker").datepicker("getDate").getTime() - 86400000)).toLocaleDateString('en-US');
@@ -86,5 +81,24 @@ mlbApp.controller('scoreboardController', ['$scope', 'getScore',
                 $('.server-message').attr("style", "display: block");
             });
         };
+       
+        var loadData = function(date) {
+            console.debug("Loading " + date);
+            getScore.get({
+                yearNum: date.getFullYear(),
+                monthNum: ("0" + (date.getMonth() + 1)).slice(-2),
+                dayNum: ("0" + date.getDate()).slice(-2)
+            }, function(res) {
+                $scope.scoreboards = updateScore(res);
+            }, function() {
+                $('.server-message').attr("style", "display: block");
+            });
+        };
+       
+
+        $interval(function()    {
+        var datetoload = $("#datepicker").datepicker("getDate");
+        loadData (new Date(datetoload));
+        }, 30000);
     }
 ]);
